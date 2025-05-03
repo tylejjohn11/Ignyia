c294789e6c9fde62230a4573ce5972ca5f40d8d2
Tyler Johnston <tylerjj01@gmail.com>
2025-05-02 21:00:45 -0500

    Quartz sync: May 2, 2025, 9:00 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,6 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
